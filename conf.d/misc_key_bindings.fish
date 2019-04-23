35bc334d41427714f1d504e088a60b19510be234
Thorsten Schubert <tschubert@bafh.org>
2019-04-23 15:52:53 +0200

    proper escaping

--- conf.d/misc_key_bindings.fish
+++ conf.d/misc_key_bindings.fish
@@ -29,7 +29,7 @@ function fzy_select_file -d 'Insert file path into the commandline buffer'
 	set -l fzy_query $commandline[2]
 	set -l fzy_args '-l 15'
 	set -q search_cmd; or set -l search_cmd "
-	command find -L $dir -mindepth 1 \( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \) -prune \
+	command find -L $dir -mindepth 1 \\( "(__fzy_get_exclusion_pattern)" -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
 	-o -type f -print \
 	-o -type d -print \
 	-o -type l -print \
@@ -60,7 +60,7 @@ function fzy_change_directory -d "change directory"
     set -l fzy_query $commandline[2]
 	set -l fzy_args '-l 15'
     set -q search_cmd; or set -l search_cmd "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    command find -L \$dir -mindepth 1 \\( "(__fzy_get_exclusion_pattern)" -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
     -o -type d -print ^/dev/null | command sed 's@^\./@@'"
 	if [ -n $fzy_query ]
 		set fzy_args "-l 15 -q $fzy_query"
@@ -68,12 +68,22 @@ function fzy_change_directory -d "change directory"
     begin
 		eval "$search_cmd | fzy $fzy_args" | read -l result
 		if [ -n "$result" ]
-			cd $result
+			echo "$result"
+			cd "$result"
 			# Remove last token from commandline.
 			commandline -t ""
 		end
     end
     commandline -f repaint
+end
+
+function __fzy_get_exclusion_pattern
+	set -l excluded_dirs .git .hg .svn .bzr .arch-ids
+	set -l epar
+	for i in $excluded_dirs
+		set -a epar "-path \$dir'*$i' -o"
+	end
+	echo (string join ' ' -- $epar)
 end
 
 function __fzy_parse_commandline -d 'Parse the current command line token and return split of existing filepath and rest of token'
@@ -85,35 +95,35 @@ function __fzy_parse_commandline -d 'Parse the current command line token and re
 		set dir '.'
 		set fzy_query ''
     else
-		set dir (__fzy_get_dir $commandline)
+		set dir (__fzy_get_dir "$commandline")
 
-		if [ "$dir" = "." -a (string sub -l 1 $commandline) != '.' ]
+		if [ "$dir" = "." -a (string sub -l 1 -- "$commandline") != '.' ]
 			# if $dir is "." but commandline is not a relative path, this means no file path found
-			set fzy_query $commandline
+			set fzy_query "$commandline"
 		else
 			# Also remove trailing slash after dir, to "split" input properly
-			set fzy_query (string replace -r "^$dir/?" '' "$commandline")
+			set fzy_query (string replace -r "^$dir/?" '' -- "$commandline")
 		end
     end
 
-    echo $dir
-    echo $fzy_query
+    echo "$dir"
+    echo "$fzy_query"
 end
 
 function __fzy_get_dir -d 'Find the longest existing filepath from input string'
     set dir $argv
 
     # Strip all trailing slashes. Ignore if $dir is root dir (/)
-    if [ (string length $dir) -gt 1 ]
-		set dir (string replace -r '/*$' '' $dir)
+    if [ (string length -- "$dir") -gt 1 ]
+		set dir (string replace -r '/*$' '' -- "$dir")
     end
 
     # Iteratively check if dir exists and strip tail end of path
     while [ ! -d "$dir" ]
 		# If path is absolute, this can keep going until ends up at /
 		# If path is relative, this can keep going until entire input is consumed, dirname returns "."
-		set dir (dirname "$dir")
+		set dir (dirname -- "$dir")
     end
 
-    echo $dir
+    echo "$dir"
 end
