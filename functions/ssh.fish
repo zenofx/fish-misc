0a0e232583e5c860b2427457b33cf76f0fdb585f
Thorsten Schubert <tschubert@bafh.org>
2019-11-29 16:42:08 +0100

    Use eval for forwarding arguments to ssh

--- functions/ssh.fish
+++ functions/ssh.fish
@@ -4,10 +4,10 @@ function ssh
 	set title (echo $argv | cut -d . -f 1)
 	if [ -n $title ]
 		tmux rename-window "{$title}"
-		command ssh "$argv"
+		eval "command ssh $argv"
 		tmux set-window-option automatic-rename "on" 1>/dev/null
 	end
   else
-    command ssh "$argv"
+    eval "command ssh $argv"
   end
 end
