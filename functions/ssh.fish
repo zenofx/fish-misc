c702917174667dc7ef7dc4cefff71b782e460d73
Thorsten Schubert <tschubert@bafh.org>
2019-12-15 04:40:33 +0100

    More complete parsing for the ssh function

--- functions/ssh.fish
+++ functions/ssh.fish
@@ -1,7 +1,11 @@
 function ssh
   set ps_res (ps -p (ps -p %self -o ppid= | xargs -n 1) -o comm=)
   if string match -q -- "tmux*" "$ps_res"
-	set title (echo $argv | cut -d . -f 1)
+	set title \
+		(string split "." -- (
+			string replace --regex -- '^(?:((?:[A-Za-z0-9_])+)\\@)?((?:[A-Za-z0-9_\\.])+)(?:\\:(\\/(?:[A-Za-z0-9_\\/]*))){0,1}$' '$2' (
+			string split " " -r -m1 -- "$argv")[-1]
+		))[1]
 	if [ -n $title ]
 		command tmux rename-window "{$title}"
 		eval "command ssh $argv"
