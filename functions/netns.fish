bea056171a96e490cba28606049b09b400bfa9d6
Thorsten Schubert <tschubert@bafh.org>
2021-07-20 02:13:00 +0200

    Add support for vivid color generator

--- functions/netns.fish
+++ functions/netns.fish
@@ -1,7 +1,7 @@
 # vim: ft=fish ts=4 sw=4 noet
 function netns --description 'launch application into network namespace'
 	if command -sq firejail
-		firejail --quiet --noprofile --netns=$argv[1] $argv[2..]
+		firejail --quiet --noprofile --rmenv=LS_COLORS --netns=$argv[1] $argv[2..]
 	else
 		sudo -E ip netns exec $argv[1] setpriv --reuid (id -u) --regid (id -g) --clear-groups --reset-env $argv[2..]
 	end
